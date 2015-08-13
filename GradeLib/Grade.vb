a7fd44a553481e8bddaf5e71b7b2aeb14b8717c5
Artit Kiuwilai <first087@gmail.com>
2015-08-13 13:32:01 +0700

    Implement check Parameter Constructor

--- GradeLib/Grade.vb
+++ GradeLib/Grade.vb
@@ -5,7 +5,8 @@ Public Class Grade
     Private _score As Integer
 
     Sub New(score As Integer)
-
+        If score < 0 Then Throw New Exception("Wrong Parameter")
+        If score > 100 Then Throw New Exception("Wrong Parameter")
     End Sub
 
     Public ReadOnly Property Score As Integer Implements IGrade.Score

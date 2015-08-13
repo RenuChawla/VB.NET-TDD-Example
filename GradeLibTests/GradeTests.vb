89996894ff8ac76a55588394179454ebd8b86347
Artit Kiuwilai <first087@gmail.com>
2015-08-13 13:13:24 +0700

    Generate Unit Test Project

---
+++ GradeLibTests/GradeTests.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Imports GradeLib
+
+Namespace GradeLib.Tests
+
+    <TestClass()> Public Class GradeTests
+
+        <TestMethod()> Public Sub NewTest()
+            Assert.Fail()
+        End Sub
+
+        <TestMethod()> Public Sub CalculateTest()
+            Assert.Fail()
+        End Sub
+    End Class
+
+
+End Namespace

424d96b0684c002d25c8f09d0201471a917caadf
Artit Kiuwilai <first087@gmail.com>
2015-08-13 13:14:34 +0700

    Naming Test Method

--- GradeLibTests/GradeTests.vb
+++ GradeLibTests/GradeTests.vb
@@ -6,11 +6,11 @@ Namespace GradeLib.Tests
 
     <TestClass()> Public Class GradeTests
 
-        <TestMethod()> Public Sub NewTest()
+        <TestMethod()> Public Sub Grade_Constructor_NotNull()
             Assert.Fail()
         End Sub
 
-        <TestMethod()> Public Sub CalculateTest()
+        <TestMethod()> Public Sub Grade_CalculateScore80_IsGradeA()
             Assert.Fail()
         End Sub
     End Class

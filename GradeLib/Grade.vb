71b97249540b3c1781914448a0ee68ff66f4fc55
Artit Kiuwilai <first087@gmail.com>
2015-08-13 12:35:53 +0700

    Add Class Grade and Implement IGrade

---
+++ GradeLib/Grade.vb
@@ -0,0 +1,20 @@
+﻿
+Public Class Grade
+    Implements IGrade
+
+    Private _score As Integer
+
+    Sub New(score As Integer)
+
+    End Sub
+
+    Public ReadOnly Property Score As Integer Implements IGrade.Score
+        Get
+
+        End Get
+    End Property
+
+    Public Function Calculate() As String Implements IGrade.Calculate
+
+    End Function
+End Class
